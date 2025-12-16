d348720d347dcef4e17e642fdb6d14fe59358601
KaranNagesh <Karannagesh86@gmail.com>
2025-12-16 22:19:08 +0530

    [Karan] : added the program of average of three numbers

---
+++ gcr codebase/program prerequisites/AverageOfThreeNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class AverageOfThreeNumber{
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	double num1 = sc.nextInt();
+	double num2 = sc.nextInt();
+	double num3 = sc.nextInt();
+	System.out.println(Average(num1,num2,num3));
+}
+    public static double Average(double num1, double num2, double num3){
+	return (num1 + num2 + num3) / 3;
+}
+
+}
+	
