8959e6498045814766714c9dbca4be5c0064e1c1
KaranNagesh <Karannagesh86@gmail.com>
2025-12-16 21:54:38 +0530

    [Karan] : adding program of calculating power

---
+++ gcr codebase/program prerequisites/PowerCalculator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class PowerCalculator{
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	double base = sc.nextInt();
+	double exponent = sc.nextInt();
+	double result = Math.pow(base,exponent);
+	System.out.println(result);
+}
+}
