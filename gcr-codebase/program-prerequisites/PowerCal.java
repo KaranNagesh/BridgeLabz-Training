7013ec7eafff8b5fa6aebb80f7ba5b00aff3e9d4
KaranNagesh <Karannagesh86@gmail.com>
2025-12-18 02:10:49 +0530

    [Karan] : added the program of power calculation

---
+++ gcr-codebase/program-prerequisites/PowerCal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class PowerCal{
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	double base = sc.nextInt();
+	double exponent = sc.nextInt();
+	double result = Math.pow(base,exponent);
+	System.out.println(result);
+}
+}
