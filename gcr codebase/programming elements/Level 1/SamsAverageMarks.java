50b26b2e5c7d89a9a85d3c64cd0f89b900651cb6
KaranNagesh <Karannagesh86@gmail.com>
2025-12-17 12:23:52 +0530

    [Karan] : added the program of calculating the average marks of sam in PCM

---
+++ gcr codebase/programming elements/Level 1/SamsAverageMarks.java
@@ -0,0 +1,9 @@
+class SamsAverageMarks{
+    public static void main(String[] args){
+	int mathsMarks = 94;
+	int physicsMarks = 95;
+	int chemistryMarks = 96;
+	int AverageMarks = (mathsMarks + physicsMarks + chemistryMarks) / 3;
+	System.out.println("Sams's average mark in PCM is " + AverageMarks);
+}
+}
