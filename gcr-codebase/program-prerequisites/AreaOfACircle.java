96759650fab74968f7fc6a749fbdc9ac4d99db38
KaranNagesh <Karannagesh86@gmail.com>
2025-12-18 01:54:23 +0530

    [Karan] : added the program of finding area of a circle

---
+++ gcr-codebase/program-prerequisites/AreaOfACircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AreaOfACircle{
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int radius = sc.nextInt();
+	System.out.println(calculateArea(radius));
+}
+    public static double calculateArea(int radius){
+	return 3.14 * radius * radius ;
+}
+}
