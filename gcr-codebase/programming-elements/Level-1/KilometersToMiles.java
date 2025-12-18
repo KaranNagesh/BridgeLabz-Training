8f8454a1ae59009d29f25be13b0a42297fed0dc7
Karan Nagesh <134263582+KaranNagesh@users.noreply.github.com>
2025-12-18 22:53:55 +0530

    Delete gcr-codebase/programming-elements/Level-1/KilometersToMiles.java

--- gcr-codebase/programming-elements/Level-1/KilometersToMiles.java
+++
@@ -1,7 +0,0 @@
-public class KilometersToMiles{
-    public static void main(String[] args){
-	double kilometers = 10.8;
-	double miles = 10.8 * 0.6;
-	System.out.println("The distance 10.8km in miles is " + miles);
-}
-}
