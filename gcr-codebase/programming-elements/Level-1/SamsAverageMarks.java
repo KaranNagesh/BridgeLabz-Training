92ae2ec3be461f01190289018fda56b9cbd444df
Karan Nagesh <134263582+KaranNagesh@users.noreply.github.com>
2025-12-18 23:00:55 +0530

    Delete gcr-codebase/programming-elements/Level-1/SamsAverageMarks.java

--- gcr-codebase/programming-elements/Level-1/SamsAverageMarks.java
+++
@@ -1,9 +0,0 @@
-class SamsAverageMarks{
-    public static void main(String[] args){
-	int mathsMarks = 94;
-	int physicsMarks = 95;
-	int chemistryMarks = 96;
-	int AverageMarks = (mathsMarks + physicsMarks + chemistryMarks) / 3;
-	System.out.println("Sams's average mark in PCM is " + AverageMarks);
-}
-}
