9df5c252fa1fb2e415ab9c0bea9bfd4cd388e690
Karan Nagesh <134263582+KaranNagesh@users.noreply.github.com>
2025-12-18 22:50:14 +0530

    Delete gcr-codebase/programming-elements/Level-1/AgeOfHarry.java

--- gcr-codebase/programming-elements/Level-1/AgeOfHarry.java
+++
@@ -1,8 +0,0 @@
-class AgeOfHarry{
-    public static void main(String[] args){
-	int birthYear = 2000;
-	int currentYear = 2024;
-	int currentAge = currentYear - birthYear ;
-	System.out.println("Harry's age in 2024 is " + currentAge);
-}
-}
