2f5398584f5bd61427628c8ec7d585c95a98e3d4
KaranNagesh <Karannagesh86@gmail.com>
2025-12-22 23:01:19 +0530

    [Karan] : added a program of number checker 4

---
+++ gcr-codebase/methods/Level-3/NumberChecker4.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class NumberChecker4 {
+
+    // Method to find sum of proper divisors of a number
+    public static int sumOfProperDivisors(int number) {
+        int sum = 0;
+
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    // Method to check Perfect Number
+    // Perfect number = sum of proper divisors equals the number
+    public static boolean isPerfectNumber(int number) {
+        return sumOfProperDivisors(number) == number;
+    }
+
+    // Method to check Abundant Number
+    // Abundant number = sum of proper divisors greater than the number
+    public static boolean isAbundantNumber(int number) {
+        return sumOfProperDivisors(number) > number;
+    }
+
+    // Method to check Deficient Number
+    // Deficient number = sum of proper divisors less than the number
+    public static boolean isDeficientNumber(int number) {
+        return sumOfProperDivisors(number) < number;
+    }
+
+    // Method to find factorial of a digit
+    public static int factorial(int digit) {
+        int fact = 1;
+        for (int i = 1; i <= digit; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    // Method to check Strong Number
+    // Strong number = sum of factorial of digits equals the number
+    public static boolean isStrongNumber(int number) {
+        int temp = number;
+        int sum = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println("Perfect Number: " + isPerfectNumber(number));
+        System.out.println("Abundant Number: " + isAbundantNumber(number));
+        System.out.println("Deficient Number: " + isDeficientNumber(number));
+        System.out.println("Strong Number: " + isStrongNumber(number));
+
+        sc.close();
+    }
+}
