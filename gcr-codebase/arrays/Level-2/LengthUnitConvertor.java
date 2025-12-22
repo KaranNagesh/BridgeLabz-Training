6504fc82b5f701aaee02af73ba4c9cc07ade81f7
KaranNagesh <Karannagesh86@gmail.com>
2025-12-22 21:30:24 +0530

    [Karan] : added a program of converting yards to feet , feet to yards , meter to inches , inches to centimeters

---
+++ gcr-codebase/arrays/Level-2/LengthUnitConvertor.java
@@ -0,0 +1,41 @@
+public class LengthUnitConvertor {
+
+    // Convert yards to feet
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    // Convert feet to yards
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    // Convert meters to inches
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    // Convert inches to meters
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    // Convert inches to centimeters
+    public static double convertInchesToCentimeters(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("5 yards to feet: " + convertYardsToFeet(5));
+        System.out.println("9 feet to yards: " + convertFeetToYards(9));
+        System.out.println("2 meters to inches: " + convertMetersToInches(2));
+        System.out.println("20 inches to meters: " + convertInchesToMeters(20));
+        System.out.println("10 inches to cm: " + convertInchesToCentimeters(10));
+    }
+}
