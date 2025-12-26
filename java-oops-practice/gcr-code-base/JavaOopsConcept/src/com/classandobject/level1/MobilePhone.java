92ce7a5168c03614444ef78c61b96f698bc4ed8d
KaranNagesh <Karannagesh86@gmail.com>
2025-12-26 16:57:04 +0530

    [Karan] : added a program to handle mobile phone details

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/classandobject/level1/MobilePhone.java
@@ -0,0 +1,21 @@
+package com.classandobject.level1;
+
+public class MobilePhone {
+	//Declaring variable for entering the details
+	String brand;
+	String model;
+	double price;
+	
+	//Constructor
+	MobilePhone(String brand, String model, double price){
+		this.brand = brand;
+		this.model = model;
+		this.price = price;
+	}
+	
+	public void displayMobile() {
+		System.out.println("Brand of mobile : " + brand );
+		System.out.println("Model of mobile : " + model);
+		System.out.println("Price of mobile : " + price);
+	}
+}
