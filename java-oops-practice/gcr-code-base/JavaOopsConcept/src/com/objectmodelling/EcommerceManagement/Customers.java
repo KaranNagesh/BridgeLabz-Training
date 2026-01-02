86d3c9241cf295d87b03d02c6cf21c0e6bed71a8
KaranNagesh <Karannagesh86@gmail.com>
2026-01-02 11:29:22 +0530

    [Karan] : added a program of ecommerce management system relationship between order , customer and product

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodelling/EcommerceManagement/Customers.java
@@ -0,0 +1,8 @@
+package com.objectmodelling.EcommerceManagement;
+//Create a class customer
+public class Customers {
+	//Public method that display order placed
+  public void placeOrder(Order order) {
+      System.out.println("Order placed successfully.");
+  }
+}
---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/objectmodelling/EcommerceManagement/Product.java
@@ -0,0 +1,12 @@
+package com.objectmodelling.EcommerceManagement;
+//Created a product class
+public class Product {
+	//Private instance of class
+  private String productName;
+
+  //Parameterized constructor
+  public Product(String productName) {
+      this.productName = productName;
+  }
+
+}
