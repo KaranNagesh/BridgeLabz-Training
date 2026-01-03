0cc432dd321c039e1435cdc9021899662c2a8631
KaranNagesh <Karannagesh86@gmail.com>
2026-01-03 11:16:46 +0530

    [Karan] : added a program of library management system.

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcept/src/com/encapsulationandpolymorphism/librarymanagement/Reservable.java
@@ -0,0 +1,12 @@
+package com.encapsulationandpolymorphism.librarymanagement;
+
+
+//Interface defining reservation-related behavior
+interface Reservable {
+
+// Reserve the library item
+void reserveItem(String borrowerName);
+
+// Check if the item is available
+boolean checkAvailability();
+}
