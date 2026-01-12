e61808a6fc894f193928816897b69c1dc0220524
KaranNagesh <Karannagesh86@gmail.com>
2026-01-12 22:31:04 +0530

    [Karan] : added a program of PetPal - Virtual Pet care App

---
+++ java-oops-practice/scenario-based/OopsScenarioBased/src/petpal/IInteractable.java
@@ -0,0 +1,10 @@
+package petpal;
+
+
+//Interface defining interactions with a pet
+interface IInteractable {
+
+void feed();   // Feed the pet
+void play();   // Play with the pet
+void sleep();  // Let the pet sleep
+}
