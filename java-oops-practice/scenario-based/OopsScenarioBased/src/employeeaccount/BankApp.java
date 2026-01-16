107de916da8c413a92e7788eddfdb762123b11a8
KaranNagesh <Karannagesh86@gmail.com>
2026-01-16 10:45:07 +0530

    [Karan] : added a program of employees account

---
+++ java-oops-practice/scenario-based/OopsScenarioBased/src/employeeaccount/BankApp.java
@@ -0,0 +1,29 @@
+
+package employeeaccount;
+ 
+public class BankApp {
+    public static void main(String[] args) {
+
+        // Creating Savings Account
+        SavingsAccount savings = new SavingsAccount(101, 5000, 5);
+
+        // Assign account to customer
+        Customer customer1 = new Customer("Karan", savings);
+
+        customer1.showBalance();
+        customer1.performDeposit(2000);
+        customer1.performWithdraw(1000);
+        savings.addInterest();
+        customer1.showBalance();
+
+        System.out.println("-------------------");
+
+        // Creating Current Account
+        CurrentAccount current = new CurrentAccount(102, 10000);
+        Customer customer2 = new Customer("Rahul", current);
+
+        customer2.showBalance();
+        customer2.performWithdraw(3000);
+        customer2.showBalance();
+    }
+}
---
+++ java-oops-practice/scenario-based/OopsScenarioBased/src/employeeaccount/CurrentAccount.java
@@ -0,0 +1,10 @@
+
+package employeeaccount;
+
+//CurrentAccount inherits Account
+public class CurrentAccount extends Account {
+
+ public CurrentAccount(int accountNumber, double balance) {
+     super(accountNumber, balance);
+ }
+}
