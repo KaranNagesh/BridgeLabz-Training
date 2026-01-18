8891678635ace7b1301bc0195aaace7c2e076221
KaranNagesh <Karannagesh86@gmail.com>
2026-01-18 22:09:57 +0530

    [Karan] : added a program of Exam Cell

---
+++ java-dsa-practice/scenario-based/DsaScenarioBased/src/examcell/Student.java
@@ -0,0 +1,17 @@
+package examcell;
+
+///Class Student to store details of students
+public class Student {
+	String name;
+	int score;
+	String testCenter;
+
+	// Parameterized Constructor
+	public Student(String name, int score, String testCenter) {
+
+		this.name = name;
+		this.score = score;
+		this.testCenter = testCenter;
+	}
+
+}
