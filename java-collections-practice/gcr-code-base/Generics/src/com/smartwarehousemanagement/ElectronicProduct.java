4396301eb13cb16bee09d779be9b67a5ea8d5ad5
KaranNagesh <Karannagesh86@gmail.com>
2026-01-18 22:56:40 +0530

    [Karan] : added a program of Smart Warehouse

---
+++ java-collections-practice/gcr-code-base/Generics/src/com/smartwarehousemanagement/ElectronicProduct.java
@@ -0,0 +1,16 @@
+package com.smartwarehousemanagement;
+
+
+//Class representing electronic items
+public class ElectronicProduct extends Product {
+
+	// Constructor for electronic product
+	public ElectronicProduct(String productName) {
+		super(productName);
+	}
+
+	// Display electronic product details
+	public void showDetails() {
+		System.out.println("Electronic Product: " + productName);
+	}
+}
---
+++ java-collections-practice/gcr-code-base/Generics/src/com/smartwarehousemanagement/FoodProduct.java
@@ -0,0 +1,16 @@
+package com.smartwarehousemanagement;
+
+
+//Class representing food items
+public class FoodProduct extends Product {
+
+	// Constructor for food product
+	public FoodProduct(String productName) {
+		super(productName);
+	}
+
+	// Display food product details
+	public void showDetails() {
+		System.out.println("Food Product: " + productName);
+	}
+}
