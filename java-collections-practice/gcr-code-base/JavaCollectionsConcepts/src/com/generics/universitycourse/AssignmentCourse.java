d244261819456d59016b20c427e3d0da2c22a2f6
KaranNagesh <Karannagesh86@gmail.com>
2026-01-19 10:48:54 +0530

    [Karan] : added a program of Multi Level University Course Management System

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/generics/universitycourse/AssignmentCourse.java
@@ -0,0 +1,11 @@
+
+package com.generics.universitycourse;
+
+//Assignment-based course type
+class AssignmentCourse extends CourseType {
+
+	// Returns evaluation type
+	public String getEvaluationType() {
+		return "Assignment-Based";
+	}
+}
---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/generics/universitycourse/ExamCourse.java
@@ -0,0 +1,11 @@
+package com.generics.universitycourse;
+
+
+//Exam-based course type
+class ExamCourse extends CourseType {
+
+	// Returns evaluation type
+	public String getEvaluationType() {
+		return "Exam-Based";
+	}
+}
