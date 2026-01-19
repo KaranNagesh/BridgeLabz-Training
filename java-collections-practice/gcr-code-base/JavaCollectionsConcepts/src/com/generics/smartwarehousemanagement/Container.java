f22964c829611377290cb1f1e7935d45dd7963cf
KaranNagesh <Karannagesh86@gmail.com>
2026-01-19 10:46:19 +0530

    [Karan] : added a program of Smart Warehouse Management

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/generics/smartwarehousemanagement/Container.java
@@ -0,0 +1,22 @@
+package com.generics.smartwarehousemanagement;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Generic container class with bounded type parameter
+public class Container<T extends Product> {
+
+	// List to store products
+	private List<T> products = new ArrayList<>();
+
+	// Method to add product to container
+	public void addProduct(T product) {
+		products.add(product);
+	}
+
+	// Method to return all products
+	public List<T> getProducts() {
+		return products;
+	}
+}
---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/generics/smartwarehousemanagement/Product.java
@@ -0,0 +1,17 @@
+package com.generics.smartwarehousemanagement;
+
+
+//Abstract base class for all products
+public abstract class Product {
+
+	// Name of the product
+	protected String productName;
+
+	// Constructor to initialize product name
+	public Product(String productName) {
+		this.productName = productName;
+	}
+
+	// Abstract method to display product details
+	public abstract void showDetails();
+}
