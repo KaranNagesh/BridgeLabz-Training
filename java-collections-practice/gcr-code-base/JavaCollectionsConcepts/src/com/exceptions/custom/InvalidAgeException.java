2afbd2c62982f90a2c627c71b097e00540b4d319
KaranNagesh <Karannagesh86@gmail.com>
2026-01-27 01:25:37 +0530

    [Karan] : added a program of exceptin handling to implement custom exception

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/exceptions/custom/InvalidAgeException.java
@@ -0,0 +1,11 @@
+package com.exceptions.custom;
+
+
+//Custom exception class
+class InvalidAgeException extends Exception {
+
+	// Constructor with custom message
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
