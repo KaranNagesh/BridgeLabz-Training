4416616b9755b259f139dcafbe3ce405bb8a31d2
KaranNagesh <Karannagesh86@gmail.com>
2026-01-28 11:59:14 +0530

    [Karan] : added the program of junit parameterized

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/regexandjunit/junit/parameterized/EvenUtils.java
@@ -0,0 +1,9 @@
+package com.regexandjunit.junit.parameterized;
+
+public class EvenUtils {
+
+	// Checks whether a number is even
+	public boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+}
