269d52d308e931f384e35f2ce7fc0de574a06249
KaranNagesh <Karannagesh86@gmail.com>
2026-01-29 23:20:18 +0530

    [Karan] : update a program of repeatable annotation

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/reflectionandannotation/annotation/repeatableannotation/SoftwareModule.java
@@ -0,0 +1,11 @@
+package com.reflectionandannotation.annotation.repeatableannotation;
+
+
+public class SoftwareModule {
+
+	@BugReport(description = "NullPointerException on startup")
+	@BugReport(description = "Memory leak in processing logic")
+	public void execute() {
+		System.out.println("Executing module...");
+	}
+}
