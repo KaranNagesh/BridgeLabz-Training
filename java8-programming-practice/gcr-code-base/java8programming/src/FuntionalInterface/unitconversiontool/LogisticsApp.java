2ee78f69f4b6129e50365b5a8a4c993e3a9ff620
KaranNagesh <Karannagesh86@gmail.com>
2026-02-01 23:47:20 +0530

    [Karan] : added a program of functional interfaces Unit conversion tool

---
+++ java8-programming-practice/gcr-code-base/java8programming/src/FuntionalInterface/unitconversiontool/LogisticsApp.java
@@ -0,0 +1,10 @@
+package FuntionalInterface.unitconversiontool;
+
+public class LogisticsApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(UnitConverter.kmToMiles(10));
+        System.out.println(UnitConverter.kgToLbs(5));
+    }
+}
