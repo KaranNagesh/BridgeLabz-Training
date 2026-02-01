03ab27a6979361e45886addc60ff8ec63b22d5e4
Karan Nagesh <134263582+KaranNagesh@users.noreply.github.com>
2026-02-01 20:31:13 +0530

    Delete java-collections-practice directory

--- java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/generics/universitycourse/CourseType.java
+++
@@ -1,8 +0,0 @@
-package com.generics.universitycourse;
-
-//Abstract base class for all course evaluation types
-abstract class CourseType {
-
-	// Returns evaluation type of the course
-	public abstract String getEvaluationType();
-}
--- java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/regexandjunit/junit/parameterized/EvenUtils.java
+++
@@ -1,9 +0,0 @@
-package com.regexandjunit.junit.parameterized;
-
-public class EvenUtils {
-
-	// Checks whether a number is even
-	public boolean isEven(int number) {
-		return number % 2 == 0;
-	}
-}
