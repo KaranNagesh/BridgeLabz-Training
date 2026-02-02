af66a6dc926ee43b8a7d4950834a239169dad04a
KaranNagesh <Karannagesh86@gmail.com>
2026-02-02 12:47:18 +0530

    [Karan] : added a program of converting json to xml

---
+++ java-io-streams/gcr-code-base/javaiostreams/src/main/java/javaiostreams/com/json/basics/JsonToXml.java
@@ -0,0 +1,20 @@
+package javaiostreams.com.json.basics;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class JsonToXml {
+	public static void main(String[] args) throws Exception {
+
+		ObjectMapper jsonMapper = new ObjectMapper(); // JSON mapper
+		XmlMapper xmlMapper = new XmlMapper(); // XML mapper
+
+		String json = "{\"name\":\"Arif\",\"age\":21,\"email\":\"arif@example.com\"}";
+
+		JsonNode node = jsonMapper.readTree(json); // parse JSON
+		String xml = xmlMapper.writeValueAsString(node); // convert to XML
+
+		System.out.println(xml); // output
+	}
+}
