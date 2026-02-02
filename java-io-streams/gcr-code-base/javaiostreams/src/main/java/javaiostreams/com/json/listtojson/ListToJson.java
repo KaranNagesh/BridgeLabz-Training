fdc87a66132ab6ae235f0b48c688f377cb3e1551
KaranNagesh <Karannagesh86@gmail.com>
2026-02-02 12:44:21 +0530

    [Karan] : added a program of converting a list to json

---
+++ java-io-streams/gcr-code-base/javaiostreams/src/main/java/javaiostreams/com/json/listtojson/ListToJson.java
@@ -0,0 +1,19 @@
+package javaiostreams.com.json.listtojson;
+
+import java.util.Arrays;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ListToJson {
+	public static void main(String[] args) throws Exception {
+
+		List<Student> students = Arrays.asList( // list of objects
+				new Student("Karan", 21), new Student("Ansh", 22));
+
+		ObjectMapper mapper = new ObjectMapper(); // JSON mapper
+
+		String jsonArray = mapper.writeValueAsString(students); // List → JSON array
+
+		System.out.println(jsonArray); // output
+	}
+}
