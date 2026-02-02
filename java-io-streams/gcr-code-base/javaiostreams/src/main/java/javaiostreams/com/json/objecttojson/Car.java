52686a1ae18c37e48f765a215e63d5490c61ef2e
KaranNagesh <Karannagesh86@gmail.com>
2026-02-02 12:50:12 +0530

    [Karan] : added a program of converting objects to json

---
+++ java-io-streams/gcr-code-base/javaiostreams/src/main/java/javaiostreams/com/json/objecttojson/Car.java
@@ -0,0 +1,28 @@
+package javaiostreams.com.json.objecttojson;
+
+//Car class
+public class Car {
+	private String brand;
+	private String model;
+	private int year;
+
+	// parameterized constructor
+	public Car(String brand, String model, int year) {
+		this.brand = brand;
+		this.model = model;
+		this.year = year;
+	}
+
+	// getters and setters
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+}
