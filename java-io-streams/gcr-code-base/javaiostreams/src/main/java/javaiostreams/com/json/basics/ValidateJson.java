1645978c43bc9cd415d8d0272980d05e4dda91a3
KaranNagesh <Karannagesh86@gmail.com>
2026-02-02 12:49:37 +0530

    [Karan] : added a program of Checking the validation of json file

---
+++ java-io-streams/gcr-code-base/javaiostreams/src/main/java/javaiostreams/com/json/basics/ValidateJson.java
@@ -0,0 +1,23 @@
+package javaiostreams.com.json.basics;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ValidateJson {
+	public static void main(String[] args) throws Exception {
+
+		ObjectMapper mapper = new ObjectMapper(); // JSON parser
+
+		String json = "{\"name\":\"Karan\",\"email\":\"Karan@example.com\"}";
+		JsonNode root = mapper.readTree(json); // parse JSON
+
+		boolean isValid = root.has("name") && // check name field
+				root.has("email"); // check email field
+
+		if (isValid) {
+			System.out.println("JSON structure is valid");
+		} else {
+			System.out.println("JSON structure is invalid");
+		}
+	}
+}
